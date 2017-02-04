9a1cd1c5cd268be0c9fb75afcc51b1e1b1dff493
ColtKing <king.colt@yahoo.ca>
2017-02-03 17:16:11 -0800

    Fixed issue #5

--- application/controllers/First.php
+++ application/controllers/First.php
@@ -5,37 +5,50 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class First extends Application
 {
     
-	function __construct()
-	{
-		parent::__construct();
-	}
+    function __construct()
+    {
+            parent::__construct();
+    }
 
-	/**
-	 * First page for our app
-	 */
-	public function index()
-	{
-            // this is the view we want shown
-            $this->data['pagebody'] = 'justone';
+    /**
+     * First page for our app
+     */
+    public function index()
+    {
+        // this is the view we want shown
+        $this->data['pagebody'] = 'justone';
 
-            // build the list of authors, to pass on to our view
-            $source = $this->quotes->get('1');
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->get('1');
 
-            $this->data = array_merge($this->data, $source);
+        $this->data = array_merge($this->data, $source);
 
-            $this->render();
-	}
+        $this->render();
+    }
 
-        public function zzz()
-	{
-            // this is the view we want shown
-            $this->data['pagebody'] = 'justone';
+    public function zzz()
+    {
+        // this is the view we want shown
+        $this->data['pagebody'] = 'justone';
 
-            // build the list of authors, to pass on to our view
-            $source = $this->quotes->get('1');
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->get('1');
 
-            $this->data = array_merge($this->data, $source);
+        $this->data = array_merge($this->data, $source);
 
-            $this->render();
-        }
+        $this->render();
+    }
+
+    public function gimme($id)
+    {
+        // this is the view we want shown
+        $this->data['pagebody'] = 'justone';
+
+        // build the list of authors, to pass on to our view
+        $source = $this->quotes->get($id);
+
+        $this->data = array_merge($this->data, $source);
+
+        $this->render();
+    }
 }
