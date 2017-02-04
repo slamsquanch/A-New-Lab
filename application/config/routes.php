2d0fbfb820b70e340982b040a15e4ad6bf356165
ColtKing <king.colt@yahoo.ca>
2017-02-03 17:41:08 -0800

    Fixed issue #7

--- application/config/routes.php
+++ application/config/routes.php
@@ -52,6 +52,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $route['default_controller'] = 'welcome';
 $route['lock/(:any)/(:any)'] =  'welcome/shucks';
 $route['sleep'] = 'first/zzz';
+$route['([a-z]{4})/bingo'] = 'bingo';
 $route['show/(:num)'] = 'first/gimme/$1';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
